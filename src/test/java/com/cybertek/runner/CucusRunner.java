a6a35217643d8702d2f8573bde13b28aeeca7786
Bakhtiar Semseddin <korshat@gmail.com>
2021-03-27 13:55:58 -0400

    Initial commit

---
+++ src/test/java/com/cybertek/runner/CucusRunner.java
@@ -0,0 +1,2 @@
+package com.cybertek.runner;public class CucusRunner {
+}
---
+++ src/test/java/com/cybertek/test/Test.java
@@ -0,0 +1,7 @@
+package com.cybertek.test;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
